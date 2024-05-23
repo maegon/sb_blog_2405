d4c2bd1a5d1b9b23910eb907bb7ce529b5091941
maegon <dlagorhs99@naver.com>
2024-05-23 16:44:02 +0900

    게시물 리ì스트 데이터 출력

--- src/main/java/com/example/blog/domain/post/controller/PostController.java
+++ src/main/java/com/example/blog/domain/post/controller/PostController.java
@@ -1,14 +1,23 @@
 package com.example.blog.domain.post.controller;
 
+import com.example.blog.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.RequiredTypes;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/post")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("postList", postService.getList());
         return "post/list";
     }
 }
