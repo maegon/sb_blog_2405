27b0bc040e6f50288deca089aa8f17832c41cf38
maegon <dlagorhs99@naver.com>
2024-05-23 15:41:54 +0900

    entity, post 기본 작업

---
+++ src/main/java/com/example/blog/global/jpa/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.example.blog.global.jpa;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+// BaseEntity가 부모 클래스가 되고 자식 클래스의 속성만 상속받아 사용
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
---
+++ src/main/java/com/example/blog/global/security/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.example.blog.global.security;
+
+
+public class SecurityConfig {
+}
